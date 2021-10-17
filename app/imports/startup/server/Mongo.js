e6c91a9e58ceafe578bf271b5dcf577d84f3fa5b
Eric Lam <53859607+airyclam@users.noreply.github.com>
2021-10-16 18:34:07 -1000

    Add anime collection and load default data

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-
+import { Anime } from '../../api/anime/Anime.js';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
@@ -15,4 +15,23 @@ if (Stuffs.collection.find().count() === 0) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
   }
+}
+
+// Initialize the database with a default data document.
+function addAnime(data) {
+  console.log(`  Adding: ${data.title}`);
+  Anime.collection.insert({
+    title: data.title,
+    img_url: data.image_url,
+    synopsis: data.synopsis,
+    episodes: data.episodes,
+    rating: data.score,
+  });
+}
+
+// Initialize the AnimeCollection if empty.
+if ((Meteor.settings.loadAssetsFile) && (Anime.collection.find().count() === 0)) {
+  console.log('Loading data anime data');
+  const jsonData = JSON.parse(Assets.getText('anime.json'));
+  jsonData.defaultAnimeData.map(data => addAnime(data));
 }
