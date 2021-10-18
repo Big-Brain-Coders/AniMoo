5843dbe6e37899d5fde1ab04c90b7d6a9404786c
Glen Larita <glarita@hawaii.edu>
2021-10-17 23:19:44 -1000

    implemented pagination

--- app/imports/ui/pages/ListAnime.jsx
+++ app/imports/ui/pages/ListAnime.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Table, Header, Loader, Pagination } from 'semantic-ui-react';
+import { Container, Table, Header, Loader, Pagination, Icon } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Anime } from '../../api/anime/Anime';
 import AnimeItem from '../components/AnimeItem';
 
+/**
+const [shows, setShows] = useState(JsonData);
+const [pageNumber, setPageNumber] = useState(0);
+const perPage = 10;
+const pagesRead = pageNumber * perPage;
+const animes = Anime.collection.find({}).fetch();
+
+const onClick = () => {
+
+};
+ * */
+
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListAnime extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+    };
+  }
+
+  handlePageChange = (e, { activePage }) => {
+    this.setState({ activePage });
+  }
 
   // If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
+
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
   // Render the page once subscriptions have been received.
   renderPage() {
+    const shows = Anime.collection.find({}).fetch();
+
     return (
       <Container>
         <Header as="h2" textAlign="center">Anime List</Header>
@@ -28,10 +53,18 @@ class ListAnime extends React.Component {
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {this.props.anime.map((show) => <AnimeItem key={show._id} anime={show} />)}
+            {this.props.anime.slice((this.state.activePage - 1) * 25, this.state.activePage * 25).map((show) => <AnimeItem key={show._id} anime={show} />)}
           </Table.Body>
         </Table>
-        <Pagination defaultActivePage={5} totalPages={10} />
+        <Pagination
+          activePage={this.activePage}
+          totalPages={Math.ceil(shows.length / 25)}
+          firstItem={{ content: <Icon name='angle double left'/>, icon: true }}
+          lastItem={{ content: <Icon name='angle double right'/>, icon: true }}
+          prevItem={{ content: <Icon name='angle left'/>, icon: true }}
+          nextItem={{ content: <Icon name='angle right'/>, icon: true }}
+          onPageChange={this.handlePageChange}
+        />
       </Container>
     );
   }
