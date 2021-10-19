67dcf6519ade83073a3351dd3fa9d227036c85af
Eric Lam <53859607+airyclam@users.noreply.github.com>
2021-10-18 15:06:14 -1000

    like button

--- app/imports/ui/components/AnimeItem.jsx
+++ app/imports/ui/components/AnimeItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Table, Image, Header } from 'semantic-ui-react';
+import { Table, Image, Header, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class AnimeItem extends React.Component {
+  addLike = () => {
+    console.log('hello');
+  };
+
   render() {
     return (
       <Table.Row>
@@ -13,6 +17,14 @@ class AnimeItem extends React.Component {
         <Table.Cell>{this.props.anime.synopsis}</Table.Cell>
         <Table.Cell textAlign='center'>{this.props.anime.episodes}</Table.Cell>
         <Table.Cell textAlign='center'>{this.props.anime.rating}</Table.Cell>
+        <Table.Cell>
+          <Button icon='heart'
+            floated='right'
+            onClick={this.addLike}
+            // label={{ basic: true, color: 'red', pointing: 'left', content: this.props.items.numberOfLike }}
+            // color={User.findOne({}).likedItems.includes(this.props.items._id) ? 'red' : null}/>
+          />
+        </Table.Cell>
       </Table.Row>
     );
   }
