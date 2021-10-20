92335c0f1827149c713dded9032860ec07db5b7c
Irene Ma <mairene@hawaii.edu>
2021-10-19 20:49:52 -1000

    fix ESLint errors

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image, Segment, Header, Card } from 'semantic-ui-react';
+import { Grid, Image, Card } from 'semantic-ui-react';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import CardCarousel from '../components/CardCarousel';
 
@@ -49,8 +49,11 @@ class Landing extends React.Component {
                 <Card.Content>
                   <Card.Header>Code Geass: Hangyaku no Lelouch</Card.Header>
                   <Card.Description>
-                    In the year 2010, the Holy Empire of Britannia is establishing itself as a dominant military nation, starting with the conquest of Japan. Renamed to Area 11 after its swift defeat, Japan has seen significant resistance against these tyrants in an attempt to regain independence.
-                   < br/> < br/>
+                    In the year 2010, the Holy Empire of Britannia is
+                    establishing itself as a dominant military nation, starting with the
+                    conquest of Japan. Renamed to Area 11 after its swift defeat, Japan has seen
+                    significant resistance against these tyrants in an attempt to regain independence.
+                    < br/> < br/>
                   </Card.Description>
                 </Card.Content>
               </Card>
@@ -63,7 +66,10 @@ class Landing extends React.Component {
                 <Card.Content>
                   <Card.Header>One Piece</Card.Header>
                   <Card.Description>
-                    Gol D. Roger was known as the "Pirate King," the strongest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Government brought a change throughout the world. His last words before his death revealed the
+                    Gol D. Roger was known as the "Pirate King," the strongest and most infamous
+                    being to have sailed the Grand Line. The capture and execution of Roger by
+                    the World Government brought a change throughout the world. His last words
+                    before his death revealed the
                     existence of the greatest treasure in the world, One Piece.
                   </Card.Description>
                 </Card.Content>
